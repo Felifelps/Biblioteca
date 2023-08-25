b40fe510e19468a9683242474de28bcf8e68f441
Felifelps <felipefelipe23456@gmail.com>
2023-08-25 10:53:15 -0300

    Starting server and templates creating

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask('Biblioteca')
+
+#Search books page
+@app.route('/search/')
+def search():
+    return '<h1>Search page</h1>'
+
+#Add books
+@app.route('/book/add/')
+def add_book():
+    return '<h1>Add books</h1>'
+
+#Edit books
+@app.route('/book/<int:id>/edit/')
+def edit_book(id):
+    return '<h1>Edit books</h1>'
+
+#Delete books
+@app.route('/book/<int:id>/delete/')
+def delete_book(id):
+    return '<h1>Delete books</h1>'
+
+#Get book information
+@app.route('/book/<int:id>/')
+def get_book(id):
+    return '<h1>Look at a book</h1>'
+
+#Add shelfs
+@app.route('/shelf/add//')
+def add_shelf():
+    return '<h1>Add shelfs</h1>'
+
+#Edit shelfs
+@app.route('/shelf/<int:id>/edit/')
+def edit_shelf(id):
+    return '<h1>Edit shelfs</h1>'
+
+#Delete shelfs
+@app.route('/shelf/<int:id>/delete/')
+def delete_shelf(id):
+    return '<h1>Delete shelfs</h1>'
+
+#Add bookcases
+@app.route('/bookcase/add//')
+def add_bookcase():
+    return '<h1>Add bookcases</h1>'
+
+#Edit bookcases
+@app.route('/bookcase/<int:id>/edit/')
+def edit_bookcase(id):
+    return '<h1>Edit bookcases</h1>'
+
+#Delete bookcases
+@app.route('/bookcase/<int:id>/delete/')
+def delete_bookcase(id):
+    return '<h1>Delete bookcases</h1>'
+
+#Main page
+@app.route('//')
+def main():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host='localhost', debug=True)
