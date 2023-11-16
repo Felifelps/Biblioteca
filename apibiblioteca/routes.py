dd0055db482cad261c46eed7650c81a48b51219f
Felifelps <felipefelipe23456@gmail.com>
2023-11-16 07:41:20 -0300

    The new book route yet

--- apibiblioteca/routes.py
+++ apibiblioteca/routes.py
@@ -205,7 +205,7 @@ async def new_book():
         json.update({
             "copies": [{"copy_id": i, "leitor": False} for i in range(1, n + 1)]
         })
-        DATA['books'].update({str(len(DATA['books'])): json})
+        DATA['books'].update({str(len(DATA['books'])) + 1: json})
         return message(f'Book{"" if n == 1 else "s"} created')
     missing_fields.pop(0)
     return message(f'Missing required parameters: {str(missing_fields)}')
